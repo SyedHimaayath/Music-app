37b53c139a97a4cb51272d98301039fd6de7f9dd
Syed Himaayath <syedhimaayath155@gmail.com>
2017-07-12 06:30:13 +0530

    all edits and changes included

--- main.js
+++ main.js
@@ -42,7 +42,8 @@
         //function to manage the play and pause functionality using the spacebar(keyboard keys)
         $('body').on('keypress',function(event){
             var target = event.target;
-            if(event.keyCode == 32 && target.tagName!='INPUT') //spacebar key holds value 32
+            if(event.keyCode == 32 && target.tagName!='INPUT') 
+            //spacebar key holds value 32
             {
                 toggleSong();
             }
@@ -84,8 +85,10 @@
                 'fileName': 'song4.mp3',
                 'image' : 'song4.jpg'
             }]
+            //click ke accordingly playlist change hoti h
         var mood1=document.querySelector('.mood1');
         mood1.addEventListener('click',function(){
+            //baaki saari detailing hide krke intented detail show krna
             $('.main').removeClass('main2')
             $('.main').removeClass('main3')
             $('.main').removeClass('main4')
@@ -94,6 +97,8 @@
             $('.main').removeClass('main7')
             $('.main').removeClass('main8')
             $('.main').addClass('main1')
+            //songs object ko override kia songs1 se
+            //first playlist
         songs = songs1 = [{
                 'name': 'Buddhu Sa Mann Hai',
                 'artist': 'Amaal Malik, Armaan Malik',
@@ -128,11 +133,13 @@
             }]
             var audio =document.querySelector('audio')
             audio.src=songs1[0].fileName
+            //mood ko select krne k turant baad playlist mey pehla song initiate hojayega
             toggleSong();
             splfunction();
         });
         var mood2=document.querySelector('.mood2');
         mood2.addEventListener('click',function(){
+            //baaki saari detailing hide krke intented detail show krna
             $('.main').removeClass('main1')
             $('.main').removeClass('main3')
             $('.main').removeClass('main4')
@@ -178,6 +185,7 @@
         });
         var mood3=document.querySelector('.mood3');
         mood3.addEventListener('click',function(){
+            //baaki saari detailing hide krke intented detail show krna
             $('.main').removeClass('main2')
             $('.main').removeClass('main1')
             $('.main').removeClass('main4')
@@ -222,6 +230,7 @@
         });
         var mood4=document.querySelector('.mood4');
         mood4.addEventListener('click',function(){
+            //baaki saari detailing hide krke intented detail show krna
             $('.main').removeClass('main1')
             $('.main').removeClass('main3')
             $('.main').removeClass('main2')
@@ -267,6 +276,7 @@
         });
         var mood5=document.querySelector('.mood5');
         mood5.addEventListener('click',function(){
+            //baaki saari detailing hide krke intented detail show krna
             $('.main').removeClass('main2')
             $('.main').removeClass('main3')
             $('.main').removeClass('main4')
@@ -310,6 +320,7 @@
         });
         var mood6=document.querySelector('.mood6');
         mood6.addEventListener('click',function(){
+            //baaki saari detailing hide krke intented detail show krna
             $('.main').removeClass('main1')
             $('.main').removeClass('main3')
             $('.main').removeClass('main4')
@@ -356,6 +367,7 @@
         });
         var mood7=document.querySelector('.mood7');
         mood7.addEventListener('click',function(){
+            //baaki saari detailing hide krke intented detail show krna
             $('.main').removeClass('main2')
             $('.main').removeClass('main3')
             $('.main').removeClass('main4')
@@ -400,6 +412,7 @@
         });
         var mood8=document.querySelector('.mood8');
         mood8.addEventListener('click',function(){
+            //baaki saari detailing hide krke intented detail show krna
             $('.main').removeClass('main1')
             $('.main').removeClass('main3')
             $('.main').removeClass('main4')
@@ -460,7 +473,7 @@
         function fancyTimeFormat(time)
         {   
             // Hours, minutes and seconds
-            var hrs = ~~(time / 3600);
+            var hrs = ~~(time / 3600);//isko use nhi kiya
             var mins = ~~((time % 3600) / 60);
             var secs = time % 60;
 
@@ -468,7 +481,7 @@
             var ret = "";
 
             if (hrs > 0) {
-                ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+                ret += "" + hrs + ":" + (mins < 10 ? "0" : ""); //ret variable mey store krliya
             }
 
             ret += "" + mins + ":" + (secs < 10 ? "0" : "");
@@ -491,13 +504,12 @@
 
         function Progressbar(bar){
           var prog = document.querySelector('.progress-filled');
-          prog.style.width= bar +"%";
-          //console.log(bar);
+          prog.style.width= bar +"%";//final width css
         }
         $('.player-progress').click(function(event){
             var $this=$(this);
-            var selectedLength= event.pageX-$this.offset().left;
-            var totalLength=$this.width();
+            var selectedLength= event.pageX-$this.offset().left;//jitni length se song play krna h
+            var totalLength=$this.width();//song ka total span
             var width=(selectedLength/totalLength)*100;
             var song=document.querySelector('audio');
             song.currentTime=(song.duration*width)/100;
@@ -508,16 +520,16 @@
         var song = document.querySelector('audio');
         values.addEventListener('change',setVolume)//watching the change
         $('.responsive-pane i').on('click',function(){
-                $('.responsive-pane i').removeClass('fa-volume-up').addClass('fa-volume-off');
-                $('.responsive-pane i').removeClass('fa-volume-down').addClass('fa-volume-off');
+                $('.responsive-pane i').removeClass('fa-volume-up').addClass('fa-volume-off');//chaining
+                $('.responsive-pane i').removeClass('fa-volume-down').addClass('fa-volume-off');//chaining
                 $('.responsive-pane span').text('Muted');
                 song.volume=0;
             console.log(song.volume);
         })
         function setVolume(){
             song.volume = this.value / 100;//converts to values in btw 0 n 1
-            if(this.value < 50){
-                $('.responsive-pane i').removeClass('fa-volume-up').addClass('fa-volume-down');
+            if(this.value < 50){//font awesome k icon change krne k liye
+                $('.responsive-pane i').removeClass('fa-volume-up').addClass('fa-volume-down');//chaining
                 $('.responsive-pane span').text(' ');
                 values=this.value;
             }
@@ -528,8 +540,8 @@
             }//changing the fa icon
         };
 
-        function changeCurrentSongDetails(songObj) {
-            $('.current-song-image').attr('src','Img/' + songObj.image)
+        function changeCurrentSongDetails(songObj) {//song k image name aur album show krn k liye
+            $('.current-song-image').attr('src','Img/' + songObj.image)//src mey value dene ka new way
             $('.current-song-name').text(songObj.name)
             $('.current-song-album').text(songObj.album)
         }
@@ -583,10 +595,10 @@ $('.fa-random').on('click',function(){
 //to keep songs continuing and checking if or not repeat is requested
 $('audio').on('ended',function(){
     var audio = document.querySelector('audio');
-    if(willLoop==0){
+    if(willLoop==0){//agar repeat icon press nhi kia hua
         if(currentSongNumber < songs.length)
         {
-            var nextSongObj = songs[currentSongNumber];
+            var nextSongObj = songs[currentSongNumber];//immediatee next song ko select krta h
             audio.src = nextSongObj.fileName;
             toggleSong();
             changeCurrentSongDetails(nextSongObj);
@@ -597,7 +609,7 @@ $('audio').on('ended',function(){
             audio.currentTime = 0;
         }
     }
-    else if(willLoop==1){
+    else if(willLoop==1){// repeat icon activate krne pr
         if(currentSongNumber < songs.length)
         {
             var nextSongObj = songs[currentSongNumber];
@@ -607,7 +619,7 @@ $('audio').on('ended',function(){
             currentSongNumber = currentSongNumber + 1;
         }
         else if(currentSongNumber = songs.length){
-            var nextSongObj = songs[0];
+            var nextSongObj = songs[0]; //phir se list k pehle wale song ko select r leta h
             audio.src = nextSongObj.fileName;
             toggleSong();
             changeCurrentSongDetails(nextSongObj);
@@ -622,15 +634,15 @@ $('audio').on('ended',function(){
 var next = document.querySelector('.fa-step-forward')
 next.addEventListener('click',function(){
     var audio=document.querySelector('audio');
-    if(songNumber<songs.length){
+    if(songNumber<songs.length){//songNumber agar limit k andar h tho next song selct karenge
         var nextSong = songs[songNumber];
         audio.src=nextSong.fileName;
         changeCurrentSongDetails(nextSong)
         songNumber++;
         toggleSong();
     }
-   else if(songNumber=songs.length){
-       changeCurrentSongDetails(songs[0])
+   else if(songNumber=songs.length){//agar song akhri h
+       changeCurrentSongDetails(songs[0])//tho phir pehla song select hojayega
        toggleSong();
         songNumber=1;
     }
@@ -639,7 +651,7 @@ next.addEventListener('click',function(){
 var previos = document.querySelector('.fa-step-backward')
 previos.addEventListener('click',function(){
     var audio=document.querySelector('audio');
-    if(songNumber>1){
+    if(songNumber>1){//ye Next ka opposie h
             var prevSong=songs[songNumber-2];
             audio.src=prevSong.fileName;
             changeCurrentSongDetails(prevSong);
@@ -656,25 +668,32 @@ previos.addEventListener('click',function(){
 })
 
 function timeJump(){var song = document.querySelector('audio');song.currentTime=song.duration-5;}
+//song ko jump krwane k liye
 
 $('#see-home').on('click',function(){
+    //selected item ko dikhane k liye
+    //baakhi saare items ko hide krdiya
     $('#mood-list').addClass('hidden')
     $('#songs_wrapper').addClass('hidden')
     $('#songs').addClass('hidden')
     $('#home-content').removeClass('hidden')
 })
 $('#see-playlist').on('click',function(){
+    //selected item ko dikhane k liye
+    //baakhi saare items ko hide krdiya
     $('#home-content').addClass('hidden')
     $('#mood-list').addClass('hidden')
      $('#songs_wrapper').removeClass('hidden')
      $('#songs').removeClass('hidden')
 })
 $('#see-moodlist').on('click',function(){
+    //selected item ko dikhane k liye
+    //baakhi saare items ko hide krdiya
     $('#home-content').addClass('hidden');
     $('#songs_wrapper').addClass('hidden');
     $('#mood-list').removeClass('hidden')
 })
-$('.mood').on('click',function(){
+$('.mood').on('click',function(){//jis element p bhi mood ki class h uspe click krne pr..
     $('.main header').removeClass('header1').addClass('header2')
     $('#mood-list').addClass('hidden')
     $('#songs_wrapper').removeClass('hidden')
